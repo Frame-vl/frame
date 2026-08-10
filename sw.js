af5dbb2a334da4d2fa64d29dc7f8367ab4aa9167
Frame-vl <n89832954726@gmail.com>
2026-08-10 18:26:45 +1000

    FRAME 2.0.1 Field Fix
    Fix camera preview, simplify dashboard, add document center, document deletion, expense logic and current objects.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='frame-v200-field';
-const ASSETS=['./','./index.html','./styles.css?v=200','./app.js?v=200','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='frame-v201-field';
+const ASSETS=['./','./index.html','./styles.css?v=201','./app.js?v=201','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting();if(event.data?.type==='CLEAR_CACHES')event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))))});
